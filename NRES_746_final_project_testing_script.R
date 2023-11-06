412a3fac7215fd21018fec41bdfca79a5578b4b9
genovamartin1 <63610881+genovamartin1@users.noreply.github.com>
2023-11-06 14:14:02 -0800

    Update NRES_746_final_project_testing_script.R

--- NRES_746_final_project_testing_script.R
+++ NRES_746_final_project_testing_script.R
@@ -1,1 +1,14 @@
 # Final Project Testing Script
+
+# Packages
+
+library(tidyverse)
+library(vegan)
+
+# Possible Data Sets
+
+# Dune vegetation and environment data from Dutch Dune Meadows
+?dune
+data(dune)
+data(dune.env)
+
