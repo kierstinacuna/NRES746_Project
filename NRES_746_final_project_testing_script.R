ed02de544e73a8a79ea184f4f38fd6ef2db9befe
genovamartin1 <63610881+genovamartin1@users.noreply.github.com>
2023-11-06 14:35:31 -0800

    Update NRES_746_final_project_testing_script.R

--- NRES_746_final_project_testing_script.R
+++ NRES_746_final_project_testing_script.R
@@ -12,3 +12,19 @@ library(vegan)
 data(dune)
 data(dune.env)
 
+hist(dune$Agrostol, freq = F)
+
+# JAGS File
+cat(
+  "model {
+  
+  # Likelihood
+  
+  for (i in 1:n.obs) {
+  exp_cover[i] <- 
+  cover_class[i] ~ dnbinom(data$cover_class, mu = exp_cover)
+  }
+  
+  # Prior
+  "
+)
