f2f08ac4918c0fd4d31d9ca806b5152e3b9c7805
kierstinacuna <kierstin.acuna@gmail.com>
2023-11-15 17:31:02 -0800

    Update Lecture_script.R

--- Lecture_script.R
+++ Lecture_script.R
@@ -1,5 +1,4 @@
 library(codep)
-library(DHARMa)
 
 # Load Doubs fish Data ----
 
@@ -7,7 +6,7 @@ data(Doubs)
 species <- as.data.frame(Doubs.fish[-8,])
 vars <- as.data.frame(cbind(Doubs.env[-8,],Doubs.geo[-8,]))
 
-# Simple linear regression
-CHA.alt <- species$CHA + 1
+# Cottus gobio model
+CHA.alt <- species$CHA + 1 #get rid of zeros
 COGO_mod <- glm(CHA.alt ~ pH + flo + oxy, data = vars, family = Gamma(link = "log"))
-summary(COGO_mod)
+summary(COGO_mod) 
