dee4a914659345e4d9c4ae40f7ba002c3965b3f9
genovamartin1 <63610881+genovamartin1@users.noreply.github.com>
2023-11-16 14:07:15 -0800

    Update Lecture_script.R

--- Lecture_script.R
+++ Lecture_script.R
@@ -65,4 +65,181 @@ calc_eu_dist <- function(spe_abun_df) {
 
 ### Breaking Down a PCA ----
 
+# Load the datasets package
+library(datasets)
 
+# Load the varechem dataset
+data(varechem)
+
+# Select data
+(data <- varechem[, 1:2])
+
+# Standardize the data to have mean zero and unit variance:
+data_std <- scale(data)
+
+# Compute the Covariance Matrix
+cov_matrix <- cov(data_std)
+
+# Perform the Eigendecomposition of the covariance matrix
+eigen_decomp <- eigen(cov_matrix)
+Eigenvalues <- eigen_decomp$values
+Eigenvectors <- eigen_decomp$vectors
+
+# Project the standardized data onto the Eigenspace
+F_PrComps <- data_std %*% Eigenvectors
+head(F_PrComps)
+
+eig_vec = Eigenvectors
+eig_val = Eigenvalues
+std_data = data_std
+
+# Plot the two principal components
+plot_2pc <- function(eig_vec, eig_val, std_data) {
+  eig_vec. <- as.data.frame(eig_vec)
+  row.names(eig_vec.) <- c("P", "N")
+  colnames(eig_vec.) <- c("PC1", "PC2")
+  eig_vec.[, 1] <- eig_vec.[, 1]*-1
+  Y_std <- as.data.frame(std_data)
+  
+  op <- par(mfrow = c(2, 1),     # 2x2 layout
+            oma = c(2, 2, 0, 0), # two rows of text at the outer left and bottom margin
+            mar = c(1, 1, 0, 0), # space for one row of text at ticks and to separate plots
+            mgp = c(2, 1, 0)    # axis label at 2 rows distance, tick labels at 1 row
+  )       
+  
+  plot(N ~ P, 
+       col = as.factor(rownames(Y_std)),
+       pch = 19, 
+       xlim=c(-2.2, 2.2), 
+       ylim = c(-2.2,2.2), 
+       data = as.data.frame(Y_std))
+  
+  abline(v=0 , h=0, 
+         col = "dark gray")
+  
+  #Overlap pertinent evectors
+  
+  abline(0, 
+         eig_vec[2, 1]/eig_vec[1, 1],
+         col='purple')
+  
+  arrows(x0 = 0, 
+         y0 = 0, 
+         x1 = eig_val[1]*eig_vec[1, 1],
+         y1 = eig_val[1]*eig_vec[2, 1],
+         col = "purple",
+         lwd = 2)
+  
+  # Plot the lines from first evector to points
+  
+  line1 <- c(0, 
+             eig_vec[2, 1]/eig_vec[1, 1])
+  
+  perp.segment.coord <- function(x0, y0, line1){
+    a <- line1[1]  #intercept
+    b <- line1[2]  #slope
+    x1 <- (x0 + b * y0 - a * b)/(1 + b^2)
+    y1 <- a + b * x1
+    list(x0 = x0, y0 = y0, 
+         x1 = x1, y1 = y1)
+  }
+  
+  ss <- perp.segment.coord(Y_std$P, 
+                           Y_std$N, 
+                           line1)
+  # do.call(segments, ss)
+  # which is the same as:
+  
+  segments(x0 = ss$x0, 
+           x1 = ss$x1, 
+           y0 = ss$y0, 
+           y1 = ss$y1, 
+           col = 'purple')
+  
+  points(N ~ P, 
+         col = as.factor(rownames(Y_std)), 
+         pch = 19,
+         data = Y_std)
+  with(Y_std,
+       text(N ~ P, 
+            labels = as.factor(rownames(Y_std)),
+            pos = 1, 
+            cex=1.4))
+  
+  
+  plot(N ~ P, 
+       col = as.factor(rownames(Y_std)),
+       pch = 19, 
+       xlim=c(-2.2, 2.2), 
+       ylim = c(-2.2,2.2), 
+       data = as.data.frame(Y_std))
+  
+  abline(v=0 , h=0, 
+         col = "dark gray")
+  
+  #Overlap pertinent evectors
+  
+  abline(0, 
+         eig_vec[2, 1]/eig_vec[1, 1],
+         col='purple')
+  abline(0, 
+         eig_vec[1, 2]/eig_vec[2, 2],
+         col='orange')
+  
+  arrows(x0 = 0, 
+        y0 = 0, 
+        x1 = eig_val[1]*eig_vec[1, 1],
+         y1 = eig_val[1]*eig_vec[2, 1],
+         col = "purple",
+         lwd = 2)
+  
+  arrows(x0 = 0, 
+         y0 = 0, 
+         x1 = eig_val[2]*eig_vec[1,2], 
+         y1 = eig_val[2]*eig_vec[2, 2],
+         col = "orange", 
+         lwd = 2)
+  
+  
+  line2 <- c(0, 
+             eig_vec[1, 2]/eig_vec[1, 1])
+  
+  perp.segment.coord <- function(x0, y0, line2){
+    a <- line2[1]  #intercept
+    b <- line2[2]  #slope
+    x1 <- (x0 + b * y0 - a * b)/(1 + b^2)
+    y1 <- a + b * x1
+    list(x0 = x0, y0 = y0, 
+         x1 = x1, y1 = y1)
+  }
+  
+  ss <- perp.segment.coord(Y_std$P, 
+                           Y_std$N, 
+                           line2)
+  
+  segments(x0 = ss$x0, 
+           x1 = ss$x1, 
+           y0 = ss$y0, 
+           y1 = ss$y1, 
+           col = 'orange')
+  
+  points(N ~ P, 
+         col = as.factor(rownames(Y_std)), 
+         pch = 19,
+         data = Y_std)
+  
+  with(Y_std,
+       text(N ~ P, 
+            labels = as.factor(rownames(Y_std)),
+            pos = 1, 
+            cex=1.4)
+  )
+  
+  title(xlab = "N",
+        ylab = "P",
+        outer = TRUE, line = 3)
+}
+
+plot_2pc(Eigenvectors, Eigenvalues, data_std)
+
+#
