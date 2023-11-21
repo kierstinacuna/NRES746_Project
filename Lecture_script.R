44ca3594da32332eee8d7c75f2bcc32349a1d20a
genovamartin1 <63610881+genovamartin1@users.noreply.github.com>
2023-11-21 11:36:03 -0800

    PCA Changes

--- Lecture_script.R
+++ Lecture_script.R
@@ -130,32 +130,22 @@ calc_bc_dist(Y.hmm)
 
 ### Breaking Down a PCA ----
 
-# Load the varechem dataset
+# 1.  Load the varechem dataset
 data(varechem)
+(data <- varechem[, 1:2]) # Select the Nitrogen and Phosphous data columns
 
-# Select data
-(data <- varechem[, 1:2])
-
-# Standardize the data to have mean zero and unit variance:
+# 2. Standardize the data to have mean zero and unit variance:
 data_std <- scale(data)
 
-# Compute the Covariance Matrix
+# 3. Compute the Covariance Matrix
 (cov_matrix <- cov(data_std))
 
-# Perform the Eigen-decomposition of the covariance matrix
+# 4. Perform the Eigen-decomposition of the covariance matrix
 eigen_decomp <- eigen(cov_matrix)
 Eigenvalues <- eigen_decomp$values
 Eigenvectors <- eigen_decomp$vectors
 
-# Project the standardized data onto the Eigen-space
-F_PrComps <- data_std %*% Eigenvectors
-head(F_PrComps)
-
-eig_vec = Eigenvectors
-eig_val = Eigenvalues
-std_data = data_std
-
-# Plot the two principal components
+# Function to plot the eigenvectors in relation to the data
 plot_2pc <- function(eig_vec, eig_val, std_data) {
   # Set Up Data and empty vectors
   eig_vec. <- as.data.frame(eig_vec)
@@ -236,3 +226,235 @@ plot_2pc <- function(eig_vec, eig_val, std_data) {
 }
 
 plot_2pc(Eigenvectors, Eigenvalues, data_std)
+
+# 5. Project the standardized data onto the Eigen-space
+F_PrComps <- data_std %*% Eigenvectors
+head(F_PrComps)
+
+
+# Trying to plot the eigen vectors
+
+
+plot(data_std[,1] ~ data_std[,2])
+
+ggplot(data.frame(data_set_2), aes(x = varc_1, y = varc_2)) +
+  geom_point(color = "blue", size = 2) +
+  geom_vline(xintercept = 0, size = .5) +
+  geom_hline(yintercept = 0, size = .5) +
+  geom_abline(slope = ev1_m, color = "blue", size = 0.7) +
+  geom_abline(slope = ev2_m, color = "red", size = 0.7) +
+  theme_classic()
+
+
+### Using blackbox PCA functions ----
+
+data(varechem)
+
+Y <- varechem[, 1:2]
+Y_std <- as.matrix(scale(Y))
+Y_R <- cov(Y_std)
+
+(Eigenvalues <- eigen(Y_R, symmetric = T)$values)
+(Eigenvectors <- eigen(Y_R, symmetric = T)$vectors)
+
+(F_PrComps <- Y_std %*% Eigenvectors)
+
+head(F_PrComps)
+
+# Using stats::prcom()
+PCA_prcomp <- stats::prcomp(Y, center = TRUE, scale = TRUE)
+
+# or PCA_prcomp <- prcomp(Y_std)
+
+(prcomp_scores <- PCA_prcomp$x)
+
+# Using stats: princomp()
+
+PCA_princomp <- stats::princomp(Y_std)
+
+head(PCA_princomp$scores)
+
+# Using vegan::rda()
+
+PCA_vegan_rda <- vegan::rda(Y_std)
+
+test <- scores(PCA_vegan_rda, display = "sites", scaling = 1, choices = seq_len(PCA_vegan_rda$CA$rank),
+       const = sqrt(PCA_vegan_rda$tot.chi * (nrow(PCA_vegan_rda$CA$u) -
+                                               1)))[1:5, ]
+
+
+### Practice using on Hellinger transformed Doubs fish data ----
+
+# Hellinger transform Doubs fish data
+spe.hel <- vegan::decostand(species, method = "hellinger")
+
+# Standardize Hellinger transformed fish data
+spe.hel.std <- as.matrix(scale(spe.hel))
+
+# Calculate covariance matrix of the fish data
+spe.cov <- cov(spe.hel.std)
+
+# Calculate the eigenvalues and eigenvectors
+spe_cov_e <- eigen(spe.cov)
+(spe.eigvec <- spe_cov_e$vectors)
+(spe.eigval <- spe_cov_e$values)
+
+
+(spe.pcscores <- spe.hel.std %*% spe.eigvec)
+
+spe.pca.vegan <- rda(spe.hel)
+summary(spe.pca.vegan)
+plot(spe.pca.vegan$CA$eig, xlim = c(0,50))
+points(spe.eigval, col = "red")
+  
+
+
+# From R-bloggers:
+# Calculate PCA from scratch
+
+library(tidyverse)
+
+set.seed(1)
+# Variable 1
+var_1 <- rnorm(50, 50, sd = 3)
+# Variable 2
+var_2 <- .5*var_1 + rnorm(50, sd = sqrt(3))
+# Both variables in a data frame
+data_set_1 <- data.frame(var_1, var_2)
+head(data_set_1)
+
+# A scatter plot with the two simulated variables
+ggplot(data_set_1, aes(x = var_1, y = var_2)) +
+  geom_point(color = "blue", size = 2) +
+  xlab("Variable 1") +
+  ylab("Variable 2") +
+  theme_classic()
+
+# Subtract the mean from each variable
+data_set_1 <- data_set_1 %>% 
+  mutate(varc_1 = var_1 - mean(var_1), varc_2 = var_2 - mean(var_2))
+head(data_set_1)
+
+# Scatter plot for the centered data 
+ggplot(data_set_1, aes(x = varc_1, y = varc_2)) +
+  geom_point(color = "blue", size = 2) +
+  geom_vline(xintercept = 0, size = .5) +
+  geom_hline(yintercept = 0, size = .5) +
+  theme_classic()
+
+# Select just the centered variables
+data_set_2 <- data_set_1 %>% 
+  select(varc_1, varc_2) %>% 
+  as.matrix()
+# Calculate the covariance matrix
+cov_m <- (t(data_set_2) %*% data_set_2) / (nrow(data_set_2) - 1) 
+cov_m
+
+# Use eigen() to obtain eigenvectors and eigenvalues
+cov_e <- eigen(cov_m)
+# Eigenvectors
+e_vec <- cov_e$vectors
+# Eigenvalues
+e_val <- cov_e$values
+
+# First eigenvector 
+ev_1 <- e_vec[,1]
+# First eigenvector slope
+ev1_m <- ev_1[2] / ev_1[1]
+# Second eigenvector 
+ev_2 <- e_vec[,2]
+# Second eigenvector slope
+ev2_m <- ev_2[2] / ev_2[1]
+# Scatter plot showing the span of both eigenvectors 
+ggplot(data.frame(data_set_2), aes(x = varc_1, y = varc_2)) +
+  geom_point(color = "blue", size = 2) +
+  geom_vline(xintercept = 0, size = .5) +
+  geom_hline(yintercept = 0, size = .5) +
+  geom_abline(slope = ev1_m, color = "blue", size = 0.7) +
+  geom_abline(slope = ev2_m, color = "red", size = 0.7) +
+  theme_classic()
+
+# Multiply both eigenvectors 
+ev_1 %*% ev_2
+##      [,1]
+## [1,]    0
+
+# Calculate the estimated variance for each eigenvalue
+e_var <- e_val / (nrow(data_set_2) - 1)
+# Data frame with variance percentages
+var_per <- data.frame(
+  PC  = c("PC1", "PC2"),
+  PER = c(e_var) * 100 / sum(e_var) # Calculate the percentage
+)
+# Scree plot 
+ggplot(var_per, aes(x = PC, y = PER)) +
+  geom_col(width = 0.5, color = "black") +
+  xlab("Principal component") +
+  ylab("Percentage of variation (%)") +
+  theme_classic()
+
+# Norm of the first eigenvector
+norm(as.matrix(ev_1), "F")
+## [1] 1
+# Norm of the second eigenvector
+norm(as.matrix(ev_2), "F")
+## [1] 1
+
+# Data frame with both eigenvectors
+loads <- data.frame(
+  VAR   = c("var_1", "var_2"),
+  PC1 = ev_1, # First eigenvecor
+  PC2 = ev_2  # Second eigenvectors
+)
+loads
+##     VAR        PC1        PC2
+## 1 var_1 -0.8134113  0.5816890
+## 2 var_2 -0.5816890 -0.8134113
+?solve
+# Inverse of eigenvectors matrix
+inv_evec <- solve(e_vec) 
+# Change the basis of the original data 
+data_set_3 <- data_set_2 %*% inv_evec
+# Scatter showing the rotation 
+ggplot(data.frame(data_set_3), aes(X1, X2)) +
+  geom_point(color = "blue", size = 2) +
+  geom_vline(xintercept = 0, size = .5) +
+  geom_hline(yintercept = 0, size = .5) +
+  xlab("PC1 (78.8%)") +
+  ylab("PC2 (21.2%)") +
+  theme_classic()
+
+library(ggpubr)
+# Scatter plot with the centered data 
+plot_data <- ggplot(data.frame(data_set_2), aes(x = varc_1, y = varc_2)) +
+  geom_point(color = "blue", size = 2) +
+  geom_vline(xintercept = 0, size = .5) +
+  geom_hline(yintercept = 0, size = .5) +
+  ylim(c(-8, 8.5)) +
+  ggtitle("Original Data") +
+  theme_classic()
+# Scatter plot with the rotated data
+plot_rotation <- ggplot(data.frame(data_set_3), aes(X1, X2)) +
+  geom_point(color = "blue", size = 2) +
+  geom_vline(xintercept = 0, size = .5) +
+  geom_hline(yintercept = 0, size = .5) +
+  xlab("PC1 (78.8%)") +
+  ylab("PC2 (21.2%)") +
+  ylim(c(-8, 8.5)) +
+  ggtitle("Change of Basis to Eigenvectors") +
+  theme_classic()
+# Both graphs side by side
+ggarrange(plot_data, plot_rotation)
+
+# Data points just from PC 1
+data_pc1 <- data.frame(v1 = data_set_3[,1], v2 = rep(0, nrow(data_set_3)))
+# Scatter plot showing the projected points from PC1 (red points)
+ggplot(data.frame(data_set_3), aes(X1, X2)) +
+  geom_point(color = "blue", size = 2) +
+  geom_point(data = data_pc1, aes(v1, v2), color = "red", size = 2) +
+  geom_vline(xintercept = 0, size = .5) +
+  geom_hline(yintercept = 0, size = .5) +
+  xlab("PC1 (78.8%)") +
+  ylab("PC2 (21.2%)") +
+  ylim(c(-8, 8.5)) +
+  theme_classic()
