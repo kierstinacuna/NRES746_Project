59a93e2d7033d36ccb6e911dccc4b4cc05f5f1dc
kierstinacuna <kierstin.acuna@gmail.com>
2023-11-21 07:19:34 -0800

    Minor edits

--- Lab_script.R
+++ Lab_script.R
@@ -1,8 +1,28 @@
-# Exercise 1
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+#$       NRES 746 Lab Script      $#
+#$       Intro to Ordination      $#
+#$ Martin Genova & Kierstin Acuna $#
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+
+# Load libraries ----
+library(codep)
+library (vegan)
+
+# Load Doubs fish Data ----
+data(Doubs)
+species <- as.data.frame(Doubs.fish[-8,])
+vars <- as.data.frame(cbind(Doubs.env[-8,],Doubs.geo[-8,]))
+
+# Exercise 1 ----
 # Build a function to calculate bray-curtis distance
 
-# Exercise 2
+# Exercise 2 ----
 # Compute the likelihood of a model matrix
 
-# Exercise 3
+# Exercise 3 ----
 # Build an RDA function
+
+# Answers ----
+## Exercise 1 ----
+## Exercise 2 ----
+## Exercise 3 ----
--- RDA_function.R
+++ RDA_function.R
@@ -13,37 +13,48 @@ data(Doubs)
 species <- as.data.frame(Doubs.fish[-8,])
 vars <- as.data.frame(cbind(Doubs.env[-8,],Doubs.geo[-8,]))
 
-# Explore data ----
-
-hist(species$CHA)
-
 # Run black box RDA ----
 
+# To simplify things, pick 3 variables to work with:
+#   flo, oxy, and pH
+
+vars <- vars[,c("flo", "oxy", "pH")]
+
 species <- decostand(species, method = "hellinger")
+# The hellinger transformation helps to account for the double zero problem, it's the 
+#   square root of the relative abundance at each site.
+# If I have them do a PCA, the data should be hellinger-transformed. But if I have them
+#   do a PCoA, they can leave the data as is, because the bray-curtis distance metric also
+#   accounts for the double-zero problem.
 
 vars <- decostand(vars, method = "standardize")
+# Continuous variables need to be scaled and centered
 
 doubs_rda <- rda(species ~ ., data = vars)
 
 summary(doubs_rda)
+# 47% of the variance is constrained, meaning 47% of the variance in Y is explained
+#   by the environmental variables in X
 
-prcomp(doubs_rda)
-
-ordiplot(doubs_rda,
-         type = "text"
-         )
+ordiplot(doubs_rda, type = "text")
 
 
 # Build RDA function ----
 
+x <- vars
+y <- species
+i=1
+
 rda_func <- function(x, y){
   # Step 1: Regress species in y over vars in x
+  
   preds <- matrix(NA, nrow = nrow(y), ncol = ncol(y))
   for (i in 1:ncol(y)) {
-    mod <- lm(y[,i] ~ x[,1] + x[,2] + x[,3] + x[,4] + x[,5] + x[,6] + x[,7] + x[,8] + x[,9] +
-                x[,10] + x[,11])
+    mod <- lm(y[,i] ~ ., data = x)
     preds[,i] <- mod$fitted.values
   }
+  colnames(preds) <- colnames(y)
+ summary(mod) 
   
   # Step 2: PCA on the fitted values
   pca <- prcomp(preds)
